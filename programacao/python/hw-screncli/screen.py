f9ced51a362b56d530795798ab32c401a7b22afd
ItamarAlves <itamaralvesprofi@gmail.com>
2021-01-15 21:03:40 -0400

    add package colorama

--- programacao/python/hw-screncli/screen.py
+++ programacao/python/hw-screncli/screen.py
@@ -1,6 +1,6 @@
 #/usr/python3
 # -*- coding: utf-8 -*-
-import click
+import click, colorama
 
 @click.command()
 @click.option("--count", default=1, help="Quantidade de vez que vai rodar o formulário")
@@ -8,6 +8,7 @@ import click
 @click.option("--sobrenome", prompt="Sobrenome", help="Sobrenome da pessoa")
 
 def cumprimento(count, nome, sobrenome):
+    click.secho('Bem Vindo a Vida!', bg="black", fg='red', bold=True)
     for _ in range(count):
         click.echo("Olá, %s, " % nome + sobrenome + "!")
 
