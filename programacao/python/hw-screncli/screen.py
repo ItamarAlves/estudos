c7ee3aaf7d5e1a8f8ab5f8da43e340b1560fb989
ItamarAlves <itamaralvesprofi@gmail.com>
2021-01-15 20:57:51 -0400

    add nome + sobrenome

--- programacao/python/hw-screncli/screen.py
+++ programacao/python/hw-screncli/screen.py
@@ -1,12 +1,15 @@
+#/usr/python3
+# -*- coding: utf-8 -*-
 import click
 
 @click.command()
-@click.option("--count", default=5, help="Número de Saudações")
-@click.option("--name", prompt="Nome", help="Pessoa que vai comprimentar")
+@click.option("--count", default=1, help="Quantidade de vez que vai rodar o formulário")
+@click.option("--nome", prompt="Nome", help="Nome da pessoa")
+@click.option("--sobrenome", prompt="Sobrenome", help="Sobrenome da pessoa")
 
-def cumprimento(count, name):
+def cumprimento(count, nome, sobrenome):
     for _ in range(count):
-        click.echo("Hello, %s!" % name)
+        click.echo("Olá, %s, " % nome + sobrenome + "!")
 
 if __name__ == '__main__':
     cumprimento()        
